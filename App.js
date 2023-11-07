07f804ffa178f78505b7c349d2053ee736618e25
citra <maharanicitra19@gmail.com>
2023-11-07 14:46:43 +0700

    update bottom tab

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { NativeBaseProvider, Text } from "native-base";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import Home from "./screens/home";
+import Profile from "./screens/profile";
+import Order from "./screens/orders";
 
-export default function App() {
+// Navigator Declaration
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const noHead = { headerShown: false };
+
+const Tabs = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: () => {
+          let iconName;
+          switch (route.name) {
+            case "Home":
+              iconName = "home-outline";
+              break;
+            case "Order":
+              iconName = "reader-outline";
+              break;
+            case "Profile":
+              iconName = "person-circle-outline";
+              break;
+          }
+          return (
+            <Ionicons
+              name={iconName}
+              size={28}
+              color={"white"}
+            />
+          );
+        },
+        tabBarIconStyle: { marginTop: 5 },
+        tabBarStyle: {
+          height: 70,
+          borderTopWidth: 0,
+          backgroundColor: "#82a9f4",
+        },
+        tabBarLabel: ({ children, color, focused }) => {
+          return (
+            <Text color={"white"} mb={2}>
+              {children}
+            </Text>
+          );
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={Home} options={noHead} />
+      <Tab.Screen name="Order" component={Order} options={noHead} />
+      <Tab.Screen name="Profile" component={Profile} options={noHead} />
+    </Tab.Navigator>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  return (
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Tabs" component={Tabs} options={noHead} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
+  );
+};
+
+export default App;
---
+++ screens/orders.js
@@ -0,0 +1,15 @@
+import { Heading, Center } from "native-base";
+import { Header } from "../components";
+
+const Order = () => {
+  return (
+    <>
+      <Header title={"Order"} />
+      <Center flex={1}>
+        <Heading>Order</Heading>
+      </Center>
+    </>
+  );
+};
+
+export default Order;
