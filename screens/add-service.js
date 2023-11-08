ab4e6635fecbee1f11f3c9a685206fa00d3cb586
Annisa Fairuz <annisafairuz77@gmail.com>
2023-11-09 00:28:54 +0700

    home

---
+++ screens/add-service.js
@@ -0,0 +1,15 @@
+import { Heading, Center } from "native-base";
+import { Header } from "../components";
+
+const AddService = () => {
+  return (
+    <>
+      <Header title={"Profile"} />
+      <Center flex={1}>
+        <Heading>AddService</Heading>
+      </Center>
+    </>
+  );
+};
+
+export default AddService;
