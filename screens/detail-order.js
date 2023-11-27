1e5700403757a8cf25ee844cef2e4eb8d0ff7194
Rafly Akbar <rafly.akbar.21@student.is.ittelkom-sby.ac.id>
2023-11-27 20:03:32 +0700

    Update screen detail-order,order dan menambahkan data dummy

--- screens/detail-order.js
+++ screens/detail-order.js
@@ -1,21 +1,32 @@
 import { Heading, Box, HStack, Text, View } from "native-base";
-import { useNavigation } from "@react-navigation/native";
-import { TouchableOpacity } from "react-native";
+import { useNavigation, route } from "@react-navigation/native";
+import { TouchableOpacity, FlatList, RefreshControl } from "react-native";
 import { Header } from "../components"
+import Orders from "./orders";
 
-const DetailOrder = () => {
+const DetailOrder = ({ route }) => {
+  const { item } = route.params;
   const navigation = useNavigation();
 
   const handleButtonPress = () => {
     navigation.navigate('ListImage'); 
   };
 
+  const calculateTotal = () => {
+    const totalHarga = item.barang.reduce((total, item) => total + item.harga, 0);
+    const totalBarang = item.barang.reduce((total, item) => {
+      total[item.namaBarang] = (total[item.namaBarang] || 0) + 1;
+      return total;
+    }, {});
+
+    return { totalHarga, totalBarang };
+  };
   return (
     <>
     <Box py={"4"} bg="#82a9f4">
     <Header withBack="true" title={"Details about"} />
         <Box mt={-3} mb={-5}>
-            <Text bold  color="white" textAlign="center" fontSize={"20"} >Order #100</Text> 
+            <Text bold  color="white" textAlign="center" fontSize={"20"} >{item.orderan}</Text> 
         </Box>
     </Box>
     <Box flex={1} bg="#82a9f4" p={10}>
@@ -25,23 +36,21 @@ const DetailOrder = () => {
             </View>
             <HStack>
               <Heading fontSize={"20"} lineHeight={"25"} ml={3}>
-                <Text style={{color:"#82a9f4"}}>WASH & IRON{"\n"}</Text>
+                <Text style={{color:"#82a9f4"}}>{item.jenisOrder}{"\n"}</Text>
               </Heading>
             </HStack>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between',  marginLeft: 11,marginRight: 11 }}>
-            <Text bold>2 x T-shirt</Text>
-            <Text>Rp 5.000</Text>
-            </View>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between',  marginLeft: 11, marginRight: 11 }}>
-            <Text bold>1 x Blazer</Text>
-            <Text>Rp 3.000</Text>
-            </View>
+            {Object.entries(calculateTotal().totalBarang).map(([namaBarang, jumlah]) => (
+              <Box key={namaBarang} style={{ flexDirection: 'row', justifyContent: 'space-between',  marginLeft: 11,marginRight: 11 }}>
+                <Text bold >{jumlah} x {namaBarang}:</Text>
+                <Text>Rp {item.barang.find(item => item.namaBarang === namaBarang)?.harga}</Text>
+              </Box>
+            ))}
             <TouchableOpacity onPress={handleButtonPress} style={{marginTop: 25, marginBottom: 25,  marginLeft: 11, }}>
                 <Text style={{color:"#82a9f4"}}>View Image</Text>
             </TouchableOpacity>
             <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 50,  marginLeft: 11, marginRight: 11 }}>
             <Text bold fontSize={"20"}>Total</Text>
-            <Text>Rp 8.000</Text>
+            <Text>Rp {calculateTotal().totalHarga}</Text>
             </View>
         </Box>
     </Box>
--- screens/orders.js
+++ screens/orders.js
@@ -1,22 +1,23 @@
 import { useNavigation } from "@react-navigation/native";
-import { Box, Button, HStack, Heading, Image, ScrollView, Text } from "native-base";
-import { useState } from "react";
+import { Box, Button, HStack, Heading, Image, ScrollView, Text, Modal } from "native-base";
+import React, { useState, useEffect } from "react";
 import { Header } from "../components";
+import DataOrder from "../DataOrder";
+import { TouchableOpacity, ActivityIndicator, RefreshControl  } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+
 
 const Orders = () => {
   const navigation = useNavigation();
-  const [isPressed, setIsPressed] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  const [data, setData] = useState(null); // Data yang akan dimuat
+  const [loading, setLoading] = useState(true); // Status loading
   
   const handleButtonPress = () => {
     navigation.navigate('DetailOrder'); 
-  };
-
-  const handlePressIn = () => {
-    setIsPressed(true);
-  };
-
-  const handlePressOut = () => {
-    setIsPressed(false);
   };
 
   const buttonStyle = {
@@ -30,8 +31,45 @@ const Orders = () => {
   };
 
   const textStyle = {
-    color: isPressed ? 'black' : '#82a9f4',
+    color: '#82a9f4',
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setData(DataOrder);
+        setLoading(false);
+      } catch (error) {
+        console.error('Data Tidak Ditemukan', error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const calculateTotalHarga = (order) => {
+    if (!order || !order.barang || order.barang.length === 0) {
+      return 0;
+    }
+
+    return order.barang.reduce((total, item) => {
+      const harga = parseFloat(item.harga);
+      return isNaN(harga) ? total : total + harga;
+    }, 0)
+  };
+
+  const [refreshing, setRefreshing] = useState(false);
+  const onRefresh = () => {
+    // Lakukan logika refresh di sini
+    setRefreshing(true);
+  
+    // Setelah proses refresh selesai, atur refreshing menjadi false
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000); // contoh: timeout 2 detik untuk simulasi refresh
+  };
+  
   return (
     <>
       <Box py={"4"} bg="#82a9f4">
@@ -42,35 +80,91 @@ const Orders = () => {
       <Box py={"5"} bg="#f6f6f6" w={"full"} borderRadius={"40"} top={"-40"} pt={"5"} pl={"10"} pr={"10"} pb={"5"}>
         <Box alignItems="flex-start" mt={5} ml={8}>
         <Button
-          onPress={() => console.log('aaaa')}
-          onPressIn={handlePressIn}
-          onPressOut={handlePressOut}
           style={buttonStyle}>
             <Text style={textStyle}>Active</Text>
         </Button>
         </Box>
-        <Box alignItems="flex-end" top={-32} mr={8}>
-          <Text> Completed</Text>
+        <Box alignItems="flex-end" top={-30} mr={8}>
+          <TouchableOpacity onPress={() => setShowModal(true)}>
+            <Text>
+              Completed
+            </Text>
+          </TouchableOpacity>
         </Box>
-        <ScrollView vertical={true} showsVerticalScrollIndicator={false}>
-          <Box p={"3"} bgColor="white" borderRadius={"10"} shadow="2" mt="5">
+        {loading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        
+        <ScrollView vertical={true} showsVerticalScrollIndicator={false} marginBottom={250}     refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+          />
+        }
+        >
+          {data && data.map((item) => (
+            <Box key={item.id} p={"3"} bgColor="white" borderRadius={"10"} mt={"2"} mb={"2"} shadow={2}>
+              <HStack>
+                <Image 
+                  source={{ uri: item.image }}
+                  alt="Gambar"
+                  size={"70"}
+                  mr={"2"}
+                />
+                <Heading p={"3"} fontSize={"20"} lineHeight={"25"}>
+                  {item.orderan} {"\n"}
+                  <Text fontSize={"15"} fontWeight={"500"}>{item.tanggal}{"\n"}</Text>
+                  <Text fontSize={"15"} fontWeight={"500"}>Rp {calculateTotalHarga(item)}</Text>
+                </Heading>
+              </HStack>
+              <Button key={item.id} onPress={() => navigation.navigate('DetailOrder', { item })} style={{ backgroundColor: "#82a9f4" }}>Order Details</Button>
+            </Box>
+          ))}
+        </ScrollView>
+      )}
+      </Box>
+      <Modal isOpen={showModal} justifyContent="flex-end" bottom="0" size="full" onClose={() => setShowModal(false)}>
+      <Modal.Content h={"full"} bg={"#f6f6f6"} py={30} borderTopRadius={40}>
+        
+        <Box alignItems="flex-start" mt={5} ml={83}>
+          <TouchableOpacity
+            onPress={() => {
+            setShowModal(false);
+            }}
+           >
+            <Text>
+              Active
+            </Text>
+          </TouchableOpacity>
+        </Box>
+        <Box alignItems="flex-end" top={-30} mr={60}>
+          <Button
+          style={buttonStyle}>
+            <Text style={textStyle}>Completed</Text>
+        </Button>
+        </Box>
+          <Modal.Body px={41}>
+          {DataOrder.map((item) => (
+          <Box key={item.id} p={"3"} bgColor="white" borderRadius={"10"} mt={"2"} mb={"2"} shadow={2}>
             <HStack>
               <Image 
-                source={require('../assets/washIron.png')}
-                alt="Alternate Text"
-                size={"79"}
+                source={{ uri: item.image }}
+                alt="Gambar"
+                size={"70"}
                 mr={"2"}
               />
               <Heading p={"3"} fontSize={"20"} lineHeight={"25"}>
-                Order #100{"\n"}
-                <Text fontSize={"15"} fontWeight={"500"}>19 Oktober 2023{"\n"}</Text>
-                <Text fontSize={"15"} fontWeight={"500"}>Rp 8.000</Text>
+                {item.orderan} {"\n"}
+                <Text fontSize={"15"} fontWeight={"500"}>{item.tanggal}{"\n"}</Text>
+                <Text fontSize={"15"} fontWeight={"500"}>Rp </Text>
               </Heading>
             </HStack>
             <Button onPress={handleButtonPress} style={{backgroundColor: "#82a9f4"}}>Order Details</Button>
           </Box>
-        </ScrollView>
-      </Box>
+          ))}
+          </Modal.Body>
+        </Modal.Content>
+      </Modal>
     </>
   );
 };
